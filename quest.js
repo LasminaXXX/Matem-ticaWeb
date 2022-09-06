9dc2f27899ecd118facc279ff1566efbcd38dd4b
LasminaXXX <104440250+LasminaXXX@users.noreply.github.com>
2022-09-05 23:21:15 -0300

    Update quest.js
    Jquery

--- quest.js
+++ quest.js
@@ -1,3 +1,21 @@
-function resposta() {
-    var correta = document.getElementById("resultado").innerHTML = ("Resposta correta é a letra B")
-}
+$(document).ready(function(){
+    $("#awnser").submit(function(e){
+        e.preventDefault();
+
+        const resultado = {
+            selecionado: $("input:checked")[0],
+            certo: $("[data-resultado='true")[0],
+        }
+
+        if(resultado.selecionado == resultado.certo){
+            $(resultado.selecionado).parent().addClass("resultado_certo");
+        }else{
+            $(resultado.selecionado).parent().addClass("resultado_errado");
+            $(resultado.certo).parent().addClass("resultado_certo");
+        }
+
+
+        $("input[type=submit]").prop("disabled", true);
+        
+    })
+});
